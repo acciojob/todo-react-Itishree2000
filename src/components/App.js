e0bdb2fd7c413e926c7c8e24fd865eb38b74098d
Itishree Jena <itishreej203@gmail.com>
2023-12-15 20:35:19 +0530

    first-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+   
+  const[text,setText]=useState([]);
+  const[inputValue,setInputValue]=useState("");
+
+  function addInputValue(event){
+    setInputValue(event.target.value);
+  }
+  
+  function handleText(){
+      if(inputValue.trim()!==('')){
+        setText([...text,inputValue]);
+        setInputValue('');
+      }
+  }
+
+  function handeleRemoveText(index){
+    const updatedText = text.filter((_, i) => i !== index);
+    setText(updatedText);
+
+    }
+  
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h3>To-Do-List</h3>
+        <input type="text" placeholder="" value={inputValue} onChange={addInputValue}/>
+        <button onClick={handleText}>Add Todo</button>
+      
+          {text.map((todo,index)=>(
+             <div key={index}>
+            {todo}
+             <button onClick={()=>handeleRemoveText(index)}>Delete</button>
+           </div>
+          ))}
+       
     </div>
   )
 }
